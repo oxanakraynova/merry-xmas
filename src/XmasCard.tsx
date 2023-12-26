7e0bba35cdaad6b3e4f80a10b1b60c020ad0b4ae
Oxana Kraynova <oxana.kraynova@dtforce.com>
2023-12-26 15:26:09 +0100

    - add snowman animation

--- src/XmasCard.tsx
+++ src/XmasCard.tsx
@@ -13,12 +13,15 @@ import StopIcon from "@mui/icons-material/Stop";
 import { useState } from "react";
 import Tooltip from "@mui/material/Tooltip";
 import AcUnitIcon from "@mui/icons-material/AcUnit";
+import snowmanAnimation from "./assets/Animation - 1703597488040.json";
+import Lottie from "lottie-react";
+import Stack from "@mui/material/Stack";
 
 const XmasCard: React.FC = () => {
   const [play, { stop }] = useSound(sound);
   const [isPlaying, setIsPlaying] = useState(false);
   const [showSnowfall, setShowSnowfall] = useState(false);
-  // const [showSanta, setShowSanta] = useState(false);
+  const [showSnowman, setShowSnowman] = useState(false);
 
   const handleCarolSinging = () => {
     if (isPlaying) {
@@ -33,15 +36,16 @@ const XmasCard: React.FC = () => {
     setShowSnowfall(!showSnowfall);
   };
 
-  // const handleSantaAppearance = () => {
-  //   setShowSanta(!showSanta);
-  // };
+  const handleSnowmanAppearance = () => {
+    setShowSnowman(!showSnowman);
+  };
 
   return (
     <Card
       sx={{
         width: "40rem",
-        height: "40rem",
+        maxHeight: "53rem",
+        overflow: "auto",
         border: "4px solid #00a152",
         borderRadius: "15px",
         boxShadow: "2px 2px 5px rgba(0, 0, 0, 0.1)",
@@ -53,28 +57,42 @@ const XmasCard: React.FC = () => {
         title="christmas tree"
       />
       <CardContent>
-        <Typography variant="h4" color="#b28900" gutterBottom>
-          Xmas Cheer for all React Girls! 🎅
+        <Typography variant="h4" color="#f6685e" gutterBottom fontWeight="bold">
+          Xmas Cheer for all React Girls!
         </Typography>
-        <Tooltip title="Add snow">
-          <IconButton color="info" onClick={handleSnowfall}>
-            <AcUnitIcon />
-          </IconButton>
-        </Tooltip>
-        {showSnowfall && <Snowfall snowflakeCount={50} style={{ zIndex: 2 }} />}
-        <Tooltip title={isPlaying ? "Turn off the music" : "Turn on the music"}>
-          <IconButton color="success" onClick={handleCarolSinging}>
-            {isPlaying ? <StopIcon /> : <PlayArrowIcon />}
-          </IconButton>
-        </Tooltip>
-        <Tooltip title="Add Santa">
-          <IconButton color="secondary">
-            {/* <IconButton color="secondary" onClick={handleSantaAppearance}> */}
-            <CardGiftcardIcon />
-          </IconButton>
-        </Tooltip>
-        {/* {showSanta && <SurpriseSanta minTime={5} maxTime={30} />} */}
-        <Typography variant="body1" gutterBottom sx={{ mt: 3 }} color="#b28900">
+        <Stack
+          direction="row"
+          spacing={2}
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            marginTop: 2,
+            marginBottom: 2,
+          }}
+        >
+          <Tooltip title={showSnowfall ? "Stop animation" : "Show Snowfall"}>
+            <IconButton color="info" onClick={handleSnowfall}>
+              {showSnowfall ? <StopIcon /> : <AcUnitIcon />}
+            </IconButton>
+          </Tooltip>
+          {showSnowfall && (
+            <Snowfall snowflakeCount={50} style={{ zIndex: 2 }} />
+          )}
+          <Tooltip
+            title={isPlaying ? "Turn off the music" : "Turn on the music"}
+          >
+            <IconButton color="success" onClick={handleCarolSinging}>
+              {isPlaying ? <StopIcon /> : <PlayArrowIcon />}
+            </IconButton>
+          </Tooltip>
+          <Tooltip title={showSnowfall ? "Stop animation" : "Show Snowman"}>
+            <IconButton color="secondary" onClick={handleSnowmanAppearance}>
+              {showSnowman ? <StopIcon /> : <CardGiftcardIcon />}
+            </IconButton>
+          </Tooltip>
+        </Stack>
+        {showSnowman && <Lottie animationData={snowmanAnimation} loop={true} />}
+        <Typography variant="body1" gutterBottom color="#357a38">
           Wishing you the merriest Christmas ever! 🎄 May it be filled with
           loads of love, laughter, and those unforgettable moments that make
           your heart glow. Here's to a season wrapped in warmth and all the good
