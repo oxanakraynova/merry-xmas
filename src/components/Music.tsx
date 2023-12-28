3f61b892a9478ec31c8b6e75eb3d8ef28643a2b7
Oxana Kraynova <oxana.kraynova@dtforce.com>
2023-12-28 18:49:23 +0100

    - update music for a better version

--- src/components/Music.tsx
+++ src/components/Music.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import useSound from "use-sound";
-import sound from "../assets/dance-of-the-sugar-plum-fairy-tchaikovsky-165957.mp3";
+import sound from "../assets/dance-of-the-sugar-plum-fairy-tchaikovsky.mp3";
 import Tooltip from "@mui/material/Tooltip";
 import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 import StopIcon from "@mui/icons-material/Stop";
